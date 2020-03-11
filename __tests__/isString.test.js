8d09dbb399e0c348b4653200fe4f43aca3b049b3
MiroslavGlamuzina <miro.glamuzina@gmail.com>
2020-03-11 11:13:19 -0700

    - Added test cases to isString()
    TODO:
    - Do stream things (at some point)

--- __tests__/isString.test.js
+++ __tests__/isString.test.js
@@ -4,10 +4,16 @@ describe('isString', () => {
 	test('Verify result of String argument', () => {
 		expect(isString('')).toEqual(true);
 		expect(isString('foobar')).toEqual(true);
+		expect(isString(new String('I Like Cheese'))).toEqual(true);
+		expect(isString('true')).toEqual(true);
+		expect(isString('123')).toEqual(true);
 	});
 	test('Ensure non-String values return false', () => {
 		expect(isString([])).toEqual(false);
 		expect(isString(false)).toEqual(false);
 		expect(isString(new Set(['foo', 'bar']))).toEqual(false);
+		expect(isString(true)).toEqual(false);
+		expect(isString(123)).toEqual(false);
+		expect(isString(10e1)).toEqual(false);
 	});
 });
